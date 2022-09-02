558babfd7cb6de742c9f5659b93d3c6857e0b5e5
Ugur Rzayev <ughurrzayev@gmail.com>
2022-09-02 11:35:14 +0400

    Task 2 done

--- task-2/main.js
+++ task-2/main.js
@@ -1,4 +1,17 @@
 // Use this array for solution
 let array = [3, 5, -7, 7, 5, -9, -4]
 
-// Write code here. 
+let arr = []
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    let varmi = false
+    for (let j = i+1; j < array.length; j++) {
+        const next_element = array[j];
+        if (element==next_element) {
+            varmi=true
+            break
+        }  
+    } if (varmi===false) {
+        arr.push(array[i])
+    }
+} console.log(arr.length);
