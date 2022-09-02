a5edb8e3992ed070eda0ae8c4ba8aa6c32b71274
Ugur Rzayev <ughurrzayev@gmail.com>
2022-09-02 11:34:21 +0400

    Task 1 done

--- task-1/main.js
+++ task-1/main.js
@@ -2,4 +2,21 @@
 let array_1 = [3, 1, 3, 4, 2, 4, 12]
 let array_2 = [4, 15, 43, 1, 15, 1]
 
-// Write code here. 
+let arr = [];
+
+for (let i = 0; i < array_1.length; i++) {
+  const element = array_1[i];
+  let varmi = false;
+
+  for (let j = 0; j < array_2.length; j++) {
+    const next_element = array_2[j];
+    if (element == next_element) {
+      varmi = true;
+      break;
+    }
+  }
+  if (varmi === false) {
+    arr.push(element);
+  }
+}
+console.log(arr);
