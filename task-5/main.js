58108b3f49d3dfbd5794f2efc5cb60e8072b4583
Ugur Rzayev <ughurrzayev@gmail.com>
2022-09-04 18:04:36 +0400

    Task 5 done

--- task-5/main.js
+++ task-5/main.js
@@ -7,4 +7,19 @@ let matrix = [
     [318, 237, 184, 36]
 ];
 
-// Write code here. 
+let j = 0
+let cem1 = 0
+let d = 0
+let cem2 = 0
+for (let i = 0; i < matrix.length; i++) {
+     const element = matrix[i];
+     const element2 = element[j];   
+     j=j+1 
+     cem1 = parseInt(cem1) + parseInt(element2)    
+    
+    }  for (let a = 3; a >= 0; a--) {
+        const element3 = matrix[d];
+        const element4 = element3[a]
+        d=d+1
+        cem2 = parseInt(cem2) + parseInt(element4)
+    } console.log(`${cem1} ${cem2}`);
